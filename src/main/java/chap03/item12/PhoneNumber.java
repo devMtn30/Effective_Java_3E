fcbff4ae07891194db3cd458c6b460b7b6aa31bd
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-09-23 15:45:51 +0900

    chapter 3 복습

---
+++ src/main/java/chap03/item12/PhoneNumber.java
@@ -0,0 +1,88 @@
+package chap03.item12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "Area code");
+        this.prefix = rangeCheck(prefix, 999, "prefix");
+        this.lineNum = rangeCheck(lineNum, 9999, "lineNum");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max) {
+            throw new IllegalArgumentException(arg + ": " + val);
+        }
+        return (short) val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if(!(o instanceof PhoneNumber))
+            return false;
+        PhoneNumber pn = (PhoneNumber) o;
+        return pn.lineNum == lineNum && pn.prefix == prefix
+                && pn.areaCode == areaCode;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Short.hashCode(areaCode);
+        result = 31 * result + Short.hashCode(prefix);
+        result = 31 * result + Short.hashCode(lineNum);
+        return result;
+    }
+
+    // hashCode 없이는 제대로 동작하지 않는다. 다음 셋 중 하나를 활성화하자.
+
+//    // 코드 11-2 전형적인 hashCode 메서드 (70쪽)
+//    @Override public int hashCode() {
+//        int result = Short.hashCode(areaCode);
+//        result = 31 * result + Short.hashCode(prefix);
+//        result = 31 * result + Short.hashCode(lineNum);
+//        return result;
+//    }
+
+//    // 코드 11-3 한 줄짜리 hashCode 메서드 - 성능이 살짝 아쉽다. (71쪽)
+//    @Override public int hashCode() {
+//        return Objects.hash(lineNum, prefix, areaCode);
+//    }
+
+//    // 해시코드를 지연 초기화하는 hashCode 메서드 - 스레드 안정성까지 고려해야 한다. (71쪽)
+//    private int hashCode; // 자동으로 0으로 초기화된다.
+//
+//    @Override public int hashCode() {
+//        int result = hashCode;
+//        if (result == 0) {
+//            result = Short.hashCode(areaCode);
+//            result = 31 * result + Short.hashCode(prefix);
+//            result = 31 * result + Short.hashCode(lineNum);
+//            hashCode = result;
+//        }
+//        return result;
+//    }
+
+    /**
+     * 이 전화번호의 문자열 표현을 반환한다.
+     * 이 문자열은 "XXX-YYY-ZZZZ" 형태의 12글자로 구성된다.
+     * XXX는 지역 코드, YYY는 프리픽스, ZZZZ는 가입자 번호다.
+     * 각각의 대문자는 10진수 숫자 하나를 나타낸다.
+     *
+     * 전화번호의 각 부분의 값이 너무 작아서 자릿수를 채울 수 없다면,
+     * 앞에서부터 0으로 채워나간다. 예컨대 가입자 번호가 123이라면
+     * 전화번호의 마지막 네 문자는 "0123"이 된다.
+     */
+    @Override public String toString() {
+        return String.format("%03d-%03d-%04d",
+                areaCode, prefix, lineNum);
+    }
+    public static void main(String[] args) {
+        PhoneNumber jenny = new PhoneNumber(707, 867, 5309);
+        System.out.println("제니의 번호: " + jenny);
+    }
+}
